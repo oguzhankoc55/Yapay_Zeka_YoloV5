77025d02cc73b0f6307b0492aab6186801834d83
Oğuzhan Koç <58952369+oguzhankoc55@users.noreply.github.com>
2023-04-25 14:44:41 +0300

    Refactor file paths

--- README.md
+++ README.md
@@ -1,10 +1,16 @@
 # Yapay_Zeka
 
+## Python Bağımlılıklarını İndirmek İçin
+
+Ana dizinde:
+```
+pip install -r yolov5/requirements.txt
+```
 ## Web Server 'ı çalıştırmak için
+Ana dizinde:
+```
+cd backend
+npm i 
+npm run start
+```
 
-### cd backend
-
-
-### npm run start
-
-### NOT: Yolo V5 çalıştırırken şuanlık Oğuzhanın dosya yolunu alıyoruz.Bu düzeltilecek.
--- backend/index.js
+++ backend/index.js
@@ -10,6 +10,7 @@ app.use(express.static('../yolov5/runs/detect/'))
 app.use(express.static(__dirname + 'public')); //Serves resources from public folder
 
 console.log("Web Server Started at : http://localhost/")
+
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/index.html'));
 })
@@ -21,23 +22,13 @@ app.post('/upload', (req, res) => {
             console.log("err")
           return res.status(500).send(err);
         }
-
-        exec("python C:/Users/oguzh/OneDrive/Belgeler/GitHub/Yapay_Zeka/yolov5/detect.py --weights C:/Users/oguzh/OneDrive/Belgeler/GitHub/Yapay_Zeka/last.pt --img 640 --conf 0.25 --source C:/Users/oguzh/OneDrive/Belgeler/GitHub/Yapay_Zeka/backend/public/a.jpeg ", (error, stdout, stderr) => {
-         
-            fs.readdir("C:/Users/oguzh/OneDrive/Belgeler/GitHub/Yapay_Zeka/yolov5/runs/detect", (err,files)=>{
-                console.log(files)
+        exec("python ../yolov5/detect.py --weights ../last.pt --img 640 --conf 0.25 --source ./public/a.jpeg ", (error, stdout, stderr) => {
+            fs.readdir("../yolov5/runs/detect", (err,files)=>{
                 var len = files.length
-                console.log(len)
                 return res.send({ path: "exp"+len.toString()});
-
             })
-
         });
-
-        
       });
 })
 
-
-// Uygulama 3000 portundan çalışacak.
 app.listen(80)
